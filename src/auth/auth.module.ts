d05b5e2660c55639539cbaffca59cf2bb52b257a
Kevin Truong <kevintruong6587@gmail.com>
2025-06-15 21:35:49 +0700

    feat: Login google

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../common/utils/constant';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '../common/guards/auth.guard';
-import { RolesGuard } from '../common/guards/role.guard';
 import { AccountService } from '../account/account.service';
 import { JwtUtilsService } from './jwtUtils.service';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [
@@ -21,7 +19,8 @@ import { JwtUtilsService } from './jwtUtils.service';
   providers: [
     AuthService,
     AccountService,
-    JwtUtilsService
+    JwtUtilsService,
+    GoogleStrategy,
   ]
 })
 export class AuthModule {}
